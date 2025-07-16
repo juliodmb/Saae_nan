276d3441e6c323bd7fea0d8a39ed481e5f29544d
juliodmb <JULIODMBARRELLA@GMAIL.COM>
2025-07-16 19:56:17 -0300

    marcador interative

--- Saae_normaliza.py
+++ Saae_normaliza.py
@@ -7,8 +7,8 @@ Created on Sun Jul 13 16:40:10 2025
 
 import pandas as pd
 import numpy as np
-from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer
+from sklearn.experimental import enable_iterative_imputer # Habilita o IterativeImputer (é uma funcionalidade experimental).
+from sklearn.impute import IterativeImputer # Importa a classe IterativeImputer para preenchimento inteligente de dados ausentes.
 from sklearn.ensemble import RandomForestRegressor
 
 # Carrega o DataFrame
